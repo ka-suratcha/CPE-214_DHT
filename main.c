b295514cf0f0b44c3b7d51ce3165458209bc439c
ka-suratcha <ka.suratcha@gmail.com>
2025-08-19 18:40:54 +0700

    re-upload main.c
    upload right main.c file

--- main.c
+++ main.c
@@ -16,191 +16,70 @@
 //for sprinf function access
 #include <stdio.h> 
 
-//for bool type
+//for boolean type
 #include <stdbool.h>
 
-//base setup
-void SystemClock_Config(void);		//max-performance config
-void GPIO_Init(void);							//config GPIO
+//clock and delay
+void SystemClock_Config(void);
+void delay40us(void) 
+{ 
+  uint32_t i = 200;
+  while(i > 0) i = i-1;
+}
 
-//delay
-void uDelay(uint32_t);
+//DHT
+void DHT_Set_Pin_Output(void);
+void DHT_Set_Pin_Input(void);
+int DHT11_read(void);
+int status = 0;
+uint32_t time_x = 0;
+int humidity, temperature;
 
-//GPIO DHT11 Config
-void DHT_Set_Pin_Output (void);		//set DHT's port to output
-void DHT_Set_Pin_Input (void);		//set DHT's port to input
+//EXTI
+void EXTI_Init(void);
 
-//DHT11 type
-struct DHT_DataTypedef 
+//button and LCD
+bool s = 0;
+char disp_str[7] = "START";
+
+//GPIO
+void GPIO_Init(void);
+
+int main()
 {
-	float Temperature;
-	float Humidity;
-};
-
-//DHT11 procress
-void DHT_start(void);													//start DHT
-uint8_t DHT_Check_Response(void);							//check DHT
-uint8_t DHT_Read(void);												//read DHT
-void DHT_GetData(struct DHT_DataTypedef *);		//getchar data for DHT
-
-struct DHT_DataTypedef DHT11_Data;		
-float T = 0;		//Temperature
-float H = 0;		//Humidity
-uint8_t H_byte1, H_byte2, T_byte1, T_byte2;
-uint16_t SUM; uint8_t Presence = 0;
-
-//user button
-bool status = 0;
-uint8_t usr_button;
-
-//LCD
-char disp_str[7];
-
-int main() {
-	SystemClock_Config();		//max-performance config
-	GPIO_Init();						//call function for GPIO Init
-	LCD_GLASS_Init();				//call function for LCD Init	
+	SystemClock_Config();														//max-performance config
+  LCD_GLASS_Init();																//LCD Init
+  DHT_Set_Pin_Output();														//set DHT's port to output
+  GPIO_Init();																		//GPIO Init
+	EXTI_Init();																		//EXTI
 	
-	
-	//DAC setup
-	RCC ->APB1ENR |= (1<<29);		//open clock for DAC
-	DAC->CR |= (1<<16);					//enable DAC channel 2
-	
-	while(1){
-		//DHT11
-		DHT_GetData(&DHT11_Data);			//run DHT11
-		T = DHT11_Data.Temperature;		//getc T
-	  H = DHT11_Data.Humidity;			//get H
-		LL_mDelay(3000);							//delay 3s
+  while(1)
+  {		
+		//DHT read and get data
+		status = DHT11_read();
+		LL_mDelay(100);
 		
-		//User button
-		usr_button = LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_0);
-		if (usr_button == 1 && !status) {
-			sprintf(disp_str,"T %.2f/n", T);
-			LCD_GLASS_DisplayString((uint8_t*)disp_str);		//diplay on LCD
-			LL_mDelay(1000);																//set delay
-			status = 1; 																		//set status to true
+		//check T is over limit?
+		if(temperature > 20){
+			LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_11);
+			LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_6);
+			DAC->DHR12R2 = 0x0FFF;
 		}
-		else if (usr_button == 1 && status) {
-			sprintf(disp_str,"H %.2f/n", H);
-			LCD_GLASS_DisplayString((uint8_t*)disp_str);		//diplay on LCD
-			LL_mDelay(1000);																//set delay
-			status = 0; 																		//set status to false
+		else{ 
+			LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_11);
+			LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_6);
+			DAC->DHR12R2 = 0;
 		}
 		
-		//Buzzer
-		if(T > 40){
-			DAC->DHR12R1 = 0x0FFF;  												//Vin = 0FFFh = 4095d
+		//show data to LCD
+		//s from EXTI
+		if(s){
+			sprintf(disp_str,"H %d", humidity);
 		}
-		
-	}
-}	
-
-void GPIO_Init(void)
-{	//setup GPIO
-		LL_GPIO_InitTypeDef GPIO_InitStruct;		//declare struct for GPIO config
-
-    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);		//open clock on AHBENR for GPIOA
-	
-		//PA0 User Button (input)
-    GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;							//set port to input
-    GPIO_InitStruct.Pin = LL_GPIO_PIN_0;										//set on pin 0
-    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;		//set output to push-pull type
-    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;									//set push-pull to no pull
-    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;				//set port to fast output speed
-    LL_GPIO_Init(GPIOA, &GPIO_InitStruct);									//config to GPIOA register
-	
-		//PA4 DHT11 (input)
-		GPIO_InitStruct.Pin = LL_GPIO_PIN_4;										//set on pin 0
-		GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;					//set port to low output speed
-    LL_GPIO_Init(GPIOA, &GPIO_InitStruct);									//config to GPIOA register
-	
-		//PA5 Buzzer (output)
-		GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;							//set port to output
-    GPIO_InitStruct.Pin = LL_GPIO_PIN_5;										//set on pin 0
-    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;				//set port to fast output speed
-    LL_GPIO_Init(GPIOA, &GPIO_InitStruct);									//config to GPIOA register
-}
-
-void DHT_Set_Pin_Output (void)
-{	//set DHT's port to output
-	LL_GPIO_InitTypeDef GPIO_InitStruct;										//declare struct for GPIO config
-	GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;							//set port to output
-	GPIO_InitStruct.Pin = LL_GPIO_PIN_4;										//set on pin 4
-	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;		//set output to push-pull type
-	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;					//set port to low output speed
-	LL_GPIO_Init(GPIOA, &GPIO_InitStruct);									//config to GPIOA register
-}
-
-void DHT_Set_Pin_Input (void)
-{	//set DHT's port to output
-	LL_GPIO_InitTypeDef GPIO_InitStruct;										//declare struct for GPIO config
-	GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;							//set port to input
-	GPIO_InitStruct.Pin = LL_GPIO_PIN_4;										//set on pin 4
-	GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;									//set push-pull to no pull
-	LL_GPIO_Init(GPIOA, &GPIO_InitStruct);									//config to GPIOA register
-}
-
-void DHT_Start(void)
-{	//start DHT
-	
-	//MCU send start signal, pull down for 18ms
-	DHT_Set_Pin_Output();														//set the pin DHT11 as output
-	
-	LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_4);		//pull the pin low
-//	LL_mDelay(6); 																	//wait for 18ms (21ms)
-	
-	//MCU pulls up for 20-40us
-	LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_4);		//pull the pin high
-//	uDelay(45); 																	//wait for 20-40us (21us)
-	
-	//ready for DHT response
-	DHT_Set_Pin_Input();		//set as input
-
-}
-
-uint8_t DHT_Read (void)
-{	//read data from DHT
-	uint8_t i,j;
-	for (j = 0; j < 8; j++)
-	{
-		while (!(LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_4)));   // wait for the pin to go high
-		uDelay(60);   // wait for 40 us (43.3us)
-		
-		if (!(LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_4)))   // if the pin is low
-		{
-			i &= ~(1<<(7-j));   // write 0
+		else{ 
+			sprintf(disp_str,"T %d", temperature);
 		}
-		else {
-			i |= (1<<(7-j));  // if the pin is high, write 1
-			while (LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_4));  // wait for the pin to go low
-		}
-	}
-	return i;
-}
-
-void DHT_GetData (struct DHT_DataTypedef *DHT_Data)
-{	//getc data from DHT
-  DHT_Start();												//run DHT11
-	
-	Presence = DHT_Check_Response();		//check DHT11 responese
-	
-	H_byte1 = DHT_Read();								//8 bit inregral RH data
-	H_byte2 = DHT_Read();								//8 bit decimal RH data
-	T_byte1 = DHT_Read();								//8 bit integral T data
-	T_byte2 = DHT_Read();								//8 bit decimal T data
-	SUM = DHT_Read();										//8 bit  checksum
-
-	if (SUM == (H_byte1+H_byte2+T_byte1+T_byte2)) {		//check data
-			DHT_Data->Temperature = T_byte1;
-			DHT_Data->Humidity = H_byte1;
-	}
-}
-
-void uDelay(uint32_t i)
-{	//delay in 2 us per unit
-	while(i != 0){
-	i -= 1;
+		LCD_GLASS_DisplayString((uint8_t*)disp_str);			//diplay on LCD
 	}
 }
 
@@ -258,46 +137,153 @@ void SystemClock_Config(void)
   LL_SetSystemCoreClock(32000000);
 }
 
-uint8_t DHT_Check_Response(void)
-{ // check DHT response
-	//	uint16_t i = 0;
-	// DHT sends out response signal and wait for 80ms
-	//	for(i = 5; i > 0; )
-	//	{
-	//		if(LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_4)) uDelay(1);	//wait to go low
-	//		else	 break;
-	//		if(--i == 0) return 0;
-	//  }
-	while (LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_4))
-		; // wait to go low
-	//	i = 10000;
-	//	while(!LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_4)){
-	//		if(i-- == 0) break;
-	//	}
+int DHT11_read(void)
+{
+  uint8_t bits[5] = {0};		//data 8 bits total 40 bits
+  uint8_t cnt = 7;					//bit position
+  uint8_t idx = 0;					//data's set
+  uint8_t i;								//loop
 
-	//	for(i = 40; i > 0; )
-	//	{
-	//		if(LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_4)) break;			//wait to go high
-	//		else	uDelay(1);
-	//		if(--i == 0) return 1;
-	//  }
-	while (!LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_4))
-		; // wait to go high
+	//DHT START
+	LL_mDelay(200);			//clear data
+	
+  DHT_Set_Pin_Output();														//set DHT's port to output
+  LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2);		//pull DHT's port to low
+  LL_mDelay(20);																	//delay for 20 ms (at least 18ms)
+  LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_2);			//set to high
+	delay40us();
+	LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2);		//pull DHT's port to low
+  DHT_Set_Pin_Input();														//set to input, ready to start DHT
 
-	//	i = 60000;
-	//	while(LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_4)){
-	//		if(i-- == 0) break;
-	//	}
-	//
+	
+	//check response DHT
+  unsigned int loopCnt = 10000;
+  while(LL_GPIO_IsInputPinSet(GPIOB, LL_GPIO_PIN_2) == 0)	//chcek low (wait to go high)
+  {
+		if(loopCnt-- == 0) return 6; //break;
+	}
+	
+	loopCnt = 30000;
+	while(LL_GPIO_IsInputPinSet(GPIOB, LL_GPIO_PIN_2) == 1)	//check high	(wait to go low)
+  {
+		if(loopCnt-- == 0) return 7; //break;
+	}
+  
+	//read data for DHT
+  for(i = 0; i < 40; i++)	//read data 40 bits
+  {
+		loopCnt = 100000;
+		time_x = 0;
+    
+		//check start transmition
+    while(LL_GPIO_IsInputPinSet(GPIOB, LL_GPIO_PIN_2) == 0)	//check low
+    {
+			if((loopCnt -= 1) == 0) return 8; //break;
+		}
+    
+		//start transmition
+    while(LL_GPIO_IsInputPinSet(GPIOB, LL_GPIO_PIN_2) == 1)	//check high
+    {
+			if((loopCnt -= 1) == 0) 
+      {
+        return idx; //break;
+      }
+			else  time_x++;	//look for high
+		}
+		
+		if (time_x > 30){
+			bits[idx] |= (1 << cnt);
+		}
+		
+    if (cnt == 0) //shift to last position?
+    {
+      cnt = 7;   //reset bit position
+      idx++;    //next data's set  			
+		}
+    else  cnt--;  //next position  
+  }
+  humidity    = (int)(bits[0]); 
+  temperature = (int)(bits[2]); 
+  
+  return 5; //break;
+}
 
-	//	for(i = 40; i > 0; )
-	//	{
-	//		if(LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_4)) uDelay(1);	//wait to go low
-	//		else						break;
-	//		if(--i == 0) return 2;
-	//  }
-	while (LL_GPIO_IsInputPinSet(GPIOA, LL_GPIO_PIN_4))
-		; // wait to go low
+void GPIO_Init(void)
+{
+		LL_GPIO_InitTypeDef GPIO_InitStruct;		         //declare struct for GPIO config
 
-	return 3;
+    LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);	//open clock on AHBENR for GPIOA
+		LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);	//open clock on AHBENR for GPIOB
+	
+		//PA0 User Button (input)
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;							//set port to input
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_0;										//set on pin 0
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;		//set output to push-pull type
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;									//set push-pull to no pull
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;				//set port to fast output speed
+    LL_GPIO_Init(GPIOA, &GPIO_InitStruct);									//config to GPIOA register
+	
+		//PA5 Buzzer (output)
+		RCC->APB1ENR |= (1<<29);		//open clock for DAC
+		GPIOA->MODER |= (3<<10); 		//alternate function	
+		DAC->CR |= (1<<16);					//enable DAC channel 2
+	
+		//PB11 LED (output)
+		GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;							//set port to input
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_11;										//set on pin 11
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;		//set output to push-pull type
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;									//set push-pull to no pull
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;				//set port to fast output speed
+    LL_GPIO_Init(GPIOA, &GPIO_InitStruct);									//config to GPIOA register
+		
+		//PB6 LED on board (output)
+		GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;							//set port to input
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_6;										//set on pin 6
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;		//set output to push-pull type
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;									//set push-pull to no pull
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;				//set port to fast output speed
+    LL_GPIO_Init(GPIOB, &GPIO_InitStruct);									//config to GPIOB register
+}
+
+void EXTI_Init(void)
+{
+		RCC->APB2ENR |= (1<<0);		//open clock for SYSCFG
+	
+		SYSCFG->EXTICR[0] &= ~(15<<0);			// EXTI Line 0 (EXTI0) for PA0
+		EXTI->RTSR |= (1<<0);								//Rising trigger slection register for PA0
+		EXTI->IMR |= (1<<0);								//Interrupt mark for PA0
+	
+		// NVIC conf
+		NVIC_EnableIRQ((IRQn_Type)6);				//use AF 6 (EXTI0)		
+		NVIC_SetPriority((IRQn_Type)6,0);		//set priority for PA0 (1st)
+}
+
+void EXTI0_IRQHandler(void)
+{		//interrupt pressed user button
+	if((EXTI->PR & (1<<0)) == 1){
+		LL_LCD_Clear();				//clear display
+		s = !s;								//Toggle s from display data on LCD
+		EXTI->PR |= (1<<0);		//reset EXTI0 for next interrupt
+	}
+}
+
+void DHT_Set_Pin_Output(void)
+{
+	LL_GPIO_InitTypeDef GPIO_InitStruct;											//declare struct for GPIO config
+	GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;								//set port to output
+	GPIO_InitStruct.Pin = LL_GPIO_PIN_2;											//set on pin 2
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;			//set output to push-pull type
+	GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;		        				//set output to push-pull type
+	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;						//set port to low output speed
+	LL_GPIO_Init(GPIOB, &GPIO_InitStruct);										//config to GPIOB register
+}
+
+void DHT_Set_Pin_Input(void)
+{
+	LL_GPIO_InitTypeDef GPIO_InitStruct;										//declare struct for GPIO config
+	GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;							//set port to output
+	GPIO_InitStruct.Pin = LL_GPIO_PIN_2;										//set on pin 2
+	GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;		        			//set output to push-pull type
+	GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;					//set port to low output speed
+	LL_GPIO_Init(GPIOB, &GPIO_InitStruct);									//config to GPIOB register
 }
